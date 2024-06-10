304dd2ab85acc4202bb08c42307662048e6a8b97
Kim Hoi Won (PMKRS) <kimhoi.won@placemakers.co.nz>
2024-06-11 09:23:04 +1200

    Updated the Contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,17 +2,59 @@
 
 import React from 'react';
 import styled from 'styled-components';
+import { FaEnvelope, FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const ContactWrapper = styled.div`
-  padding: 2rem;
+  padding: 4rem 2rem;
   text-align: center;
+  background-color: #f4f4f9;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+  color: #333;
+  margin-bottom: 1.5rem;
+`;
+
+const ContactInfo = styled.div`
+  margin-bottom: 1.5rem;
+`;
+
+const ContactLink = styled.a`
+  display: inline-block;
+  font-size: 1.2rem;
+  color: #007bff;
+  text-decoration: none;
+  margin: 0.5rem;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Icon = styled.span`
+  margin-right: 0.5rem;
 `;
 
 function Contact() {
   return (
     <ContactWrapper>
-      <h1>Contact</h1>
-      <p>Contact form or email, LinkedIn, GitHub, etc.</p>
+      <Title>Contact</Title>
+      <ContactInfo>
+        <Icon><FaEnvelope /></Icon>
+        <ContactLink href="mailto:gw31513@gmail.com">gw31513@gmail.com</ContactLink>
+      </ContactInfo>
+      <ContactInfo>
+        <Icon><FaLinkedin /></Icon>
+        <ContactLink href="https://www.linkedin.com/in/hoi-won-kim-4bbb16263/?trk=people_directory&original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=nz" target="_blank" rel="noopener noreferrer">
+          LinkedIn
+        </ContactLink>
+      </ContactInfo>
+      <ContactInfo>
+        <Icon><FaGithub /></Icon>
+        <ContactLink href="https://github.com/hoiwonkim" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </ContactLink>
+      </ContactInfo>
     </ContactWrapper>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -65,6 +65,17 @@ const CertImage = styled.img`
   margin-bottom: 0; // 간격을 없애기 위해 margin-bottom을 0으로 설정합니다.
 `;
 
+const CertLink = styled.a`
+  display: block;
+  margin-bottom: 1rem;
+  font-size: 1.2rem;
+  color: #007bff;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
 const Home = () => {
   return (
     <HomeWrapper>
@@ -84,6 +95,48 @@ const Home = () => {
         <CertImage src={cert4} alt="Certificate 4" />
         <CertImage src={cert5} alt="Certificate 5" />
         <CertImage src={cert6} alt="Certificate 6" />
+        <CertLink href="https://nomadcoders.co/certs/365364fb-1893-43fc-b557-143b924588c1" target="_blank" rel="noopener noreferrer">
+          Nomad Coders JavaScript Challenge 2020
+        </CertLink>
+        <CertLink href="https://nomadcoders.co/certs/e4e74b3b-c494-4e8d-aa31-4a4e0f1373a8" target="_blank" rel="noopener noreferrer">
+          Nomad Coders React Native Challenge 2020
+        </CertLink>
+        <CertLink href="https://nomadcoders.co/certs/0e35edca-1201-481e-aea3-77ef14a016ee" target="_blank" rel="noopener noreferrer">
+          Nomad Coders Python Challenge 2020
+        </CertLink>
+        <CertLink href="https://nomadcoders.co/certs/b4ab9599-6da8-499e-a705-145d0a99b846" target="_blank" rel="noopener noreferrer">
+          Nomad Coders Node.js Challenge 2020
+        </CertLink>
+        <CertLink href="https://nomadcoders.co/certs/ba79e7b7-9c85-4be8-9598-d521e56e290a" target="_blank" rel="noopener noreferrer">
+          Nomad Coders CSS Masterclass 2020
+        </CertLink>
+        <CertLink href="https://nomadcoders.co/certs/6378344d-d925-4ad9-ab5b-450513405c8e" target="_blank" rel="noopener noreferrer">
+          Nomad Coders Vanilla JS Challenge 2020
+        </CertLink>
+        <CertLink href="https://nomadcoders.co/certs/5ca20420-4250-4d89-88b8-e133f4ed32a7" target="_blank" rel="noopener noreferrer">
+          Nomad Coders GraphQL Challenge 2020
+        </CertLink>
+        <CertLink href="https://nomadcoders.co/certs/d0e8ab57-87c2-4d58-9b42-73402bb22d30" target="_blank" rel="noopener noreferrer">
+          Nomad Coders TypeScript Challenge 2020
+        </CertLink>
+        <CertLink href="https://nomadcoders.co/certs/4cb5fbb1-b260-49ce-8443-9c6032baf015" target="_blank" rel="noopener noreferrer">
+          Nomad Coders PHP Challenge 2020
+        </CertLink>
+        <CertLink href="https://nomadcoders.co/certs/deb2a050-18a9-4eb7-8b70-bc6f11f41a0e" target="_blank" rel="noopener noreferrer">
+          Nomad Coders MongoDB Challenge 2020
+        </CertLink>
+        <CertLink href="https://nomadcoders.co/certs/c77ba1a0-8bda-4321-a224-30aff9e779f3" target="_blank" rel="noopener noreferrer">
+          Nomad Coders SQL Challenge 2020
+        </CertLink>
+        <CertLink href="https://nomadcoders.co/certs/56109d35-a2dd-4451-bd43-7db013dcf45e" target="_blank" rel="noopener noreferrer">
+          Nomad Coders Docker Challenge 2020
+        </CertLink>
+        <CertLink href="https://nomadcoders.co/certs/89286432-1a72-4b1e-bc08-97f69034d6f2" target="_blank" rel="noopener noreferrer">
+          Nomad Coders Firebase Challenge 2020
+        </CertLink>
+        <CertLink href="https://nomadcoders.co/certs/315ba60a-68c0-4520-8759-61f826b5ec10" target="_blank" rel="noopener noreferrer">
+          Nomad Coders Redux Challenge 2020
+        </CertLink>
       </Section>
       <Section>
         <SectionTitle>Education</SectionTitle>
